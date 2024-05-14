bde15de0425309c91fc9bddaa315cc4a7363dc5f
heesungjang <heesungj7@gmail.com>
2024-05-15 06:50:58 +1000

    pass 2st stage

--- app/main.ts
+++ app/main.ts
@@ -9,5 +9,7 @@ console.log('Logs from your program will appear here!');
 
 // Uncomment this to pass the first stage
 server.listen(4221, 'localhost', () => {
-  console.log('Server is running on port 4221');
+  const res = 'HTTP/1.1 200 OK\r\n\r\n';
+
+  return res; // ??
 });
